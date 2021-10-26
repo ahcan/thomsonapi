c6d65b6a6a5bf89bb9fc0725a62e69befa335460
Huy Nguyen <tronghuy02@gmail.com>
2021-10-26 16:44:22 +0700

    add dependentcy

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ThomsonApi",
-    version="0.0.1",
+    version="0.0.2",
     author="Thuong Huynh",
     author_email="hhthuongbtr@gmail.com",
     description="Thomson API",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/hhthuongbtr/thomson.git",
+    install_requires=["requests"],
     packages=setuptools.find_packages(),
     classifiers=(
-        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
